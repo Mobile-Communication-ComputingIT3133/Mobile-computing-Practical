9d17cc9aaec5dc18ecb25ac744ac9db80c32926c
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2024-11-22 11:02:06 +0000

    react component added and import

--- day07/src/App.js
+++ day07/src/App.js
@@ -11,7 +11,9 @@ function App() {
 
   return (
     <div className="App">
-      
+      <h2>Hello React I'm Parent</h2>
+      <Childcom setfun={receiveName}/>
+      <p>Parent:My child said:{childname}</p>
     </div>
   );
 }
